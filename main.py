7b10054f8a44dcfadd37a12c46f04e0f7cd98f9c
vadim <vadymshkoda2005@gmail.com>
2023-06-07 13:42:34 +0300

    Створив файл, написав функцію для шифрування шифром Цезаря

---
+++ main.py
@@ -0,0 +1,12 @@
+#Функція для шифрування тексту шифром Цезаря:
+def encrypt_caesar(plaintext, shift):
+    encrypted_text = ""
+    for char in plaintext:
+        if char.isalpha():
+            if char.islower():
+                encrypted_text += chr((ord(char) - ord('a') + shift) % 26 + ord('a'))
+            else:
+                encrypted_text += chr((ord(char) - ord('A') + shift) % 26 + ord('A'))
+        else:
+            encrypted_text += char
+    return encrypted_text
